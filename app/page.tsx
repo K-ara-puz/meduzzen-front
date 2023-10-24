298e1145c2a1835e1830fea147057ee15424befe
K-ara-puz <mis.viktoriya@hotmail.com>
2023-10-24 16:15:00 +0300

    app init

---
+++ app/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <h1>This is meduzzen-front project based on Next JS!</h1>
+  )
+}
---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextBuildId = require('next-build-id');
+const nextConfig = {
+  generateBuildId: () => nextBuildId({ dir: __dirname, describe: true })
+}
+
+module.exports = nextConfig
