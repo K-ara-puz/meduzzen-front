e938aa42966aaf2b6fcd0e48ea7a08ed3e41c90e
K-ara-puz <mis.viktoriya@hotmail.com>
2023-11-02 13:14:03 +0200

    added redux statement

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,12 @@
 "use client"
-import Link from "next/link";
+import { MainRouter } from "@/components/MainRouter";
+import { Provider } from "react-redux";
+import store from "./store/store";
 
 export default function Home() {
   return (
-    <main>
-      <nav className="bg-red-500 w-w m-5 p-5 flex justify-between flex-wrap">
-        <Link href='/about' className="underline">About</Link>
-        <Link href='/profile' className="underline">Profile</Link>
-        <Link href='/users' className="underline">Users</Link>
-        <Link href='/companies' className="underline">Companies</Link>
-        <Link href='/login' className="underline">Login</Link>
-        <Link href='/registration' className="underline">Register</Link>
-      </nav>
-    </main>
-  )
+    <Provider store={store}>
+      <MainRouter></MainRouter>
+    </Provider>
+  );
 }
